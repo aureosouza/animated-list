cc038340a224844993c95db683d92f978ed90ffa
Aureo Beck <aureosouza0801@gmail.com>
2023-06-26 15:57:58 +0200

    Update data

--- App.tsx
+++ App.tsx
@@ -10,37 +10,10 @@ import Animated, {
 
 const {height: SCREEN_HEIGHT} = Dimensions.get('screen');
 
-const data = [
-  {id: '1', text: 'Item 1'},
-  {id: '2', text: 'Item 2'},
-  {id: '3', text: 'Item 3'},
-  {id: '4', text: 'Item 4'},
-  {id: '5', text: 'Item 5'},
-  {id: '6', text: 'Item 6'},
-  {id: '7', text: 'Item 7'},
-  {id: '8', text: 'Item 8'},
-  {id: '9', text: 'Item 9'},
-  {id: '10', text: 'Item 10'},
-  {id: '11', text: 'Item 11'},
-  {id: '12', text: 'Item 12'},
-  {id: '13', text: 'Item 13'},
-  {id: '14', text: 'Item 14'},
-  {id: '15', text: 'Item 15'},
-  {id: '16', text: 'Item 16'},
-  {id: '17', text: 'Item 17'},
-  {id: '18', text: 'Item 18'},
-  {id: '19', text: 'Item 19'},
-  {id: '20', text: 'Item 20'},
-  {id: '21', text: 'Item 21'},
-  {id: '22', text: 'Item 22'},
-  {id: '23', text: 'Item 23'},
-  {id: '24', text: 'Item 24'},
-  {id: '25', text: 'Item 25'},
-  {id: '26', text: 'Item 26'},
-  {id: '27', text: 'Item 27'},
-  {id: '28', text: 'Item 28'},
-  {id: '29', text: 'Item 29'},
-];
+const data = Array.from({length: 30}, (_, index) => ({
+  id: `${index + 1}`,
+  text: `Item ${index + 1}`,
+}));
 
 const App = () => {
   const animatedVerticalScroll = useSharedValue(0);
